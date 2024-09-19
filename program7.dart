7d1571e591d5a59b47a03d028e08c4401c2666d8
Nikhil Deshmane <iamdeshmanenl@gmail.com>
2024-09-19 18:12:28 +0530

    Add files via upload

---
+++ program7.dart
@@ -0,0 +1,7 @@
+void main(){
+  int table=12;
+  for(int i=1;i<11;i++){
+    int ans=table*i;
+    print(ans);
+  }
+}
---
+++ program8.dart
@@ -0,0 +1,7 @@
+void main(){
+int table=12;
+for(int i=10;i>0;i--){
+  int ans=table*i;
+  print(ans);
+}
+}
